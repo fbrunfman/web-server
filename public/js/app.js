7d8c02d05461afe91b168612bf2d9572f238b115
Federico <federicobrunfman@gmail.com>
2019-07-15 21:58:04 -0300

    connection with production

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ weatherForm.addEventListener('click', (e) => {
   e.preventDefault()
   const location = search.value
   console.log(location);
-  fetch('http://localhost:3000/weather?address=' + location ).then((response) => {
+  fetch('/weather?address=' + location ).then((response) => {
   response.json().then((data) => {
     if (data.error) {
       console.log(data.error);
